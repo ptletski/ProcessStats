4924066633e38974ad434579e39a6c8938467c3d
Paul Tletski <ptletski@cox.net>
2020-03-05 11:11:42 -0500

    Updates

--- ProcessStat/Models-Resources/ProcessStats.cs
+++ ProcessStat/Models-Resources/ProcessStats.cs
@@ -3,8 +3,11 @@ namespace Models_Resources
 {
     public class ProcessStats
     {
-        public int Memsize { get; set; }
         public int ProcessId { get; set; }
         public string ProcessName { get; set; }
+        public long WorkingSet { get; set; }
+        public long WorkingSetPrivate { get; set; }
+        public long PrivateBytes { get; set; }
+        public long VirtualBytes { get; set; }
     }
 }
--- ProcessStat/Models-Resources/ProcessStatsResourcer.cs
+++ ProcessStat/Models-Resources/ProcessStatsResourcer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Models_Resources
@@ -12,9 +13,15 @@ namespace Models_Resources
 
             try
             {
-                PerformanceCounter pc = new PerformanceCounter();
                 Process currentProcess = Process.GetCurrentProcess();
-
+                string[] counters = new string[] {
+                    "Working Set",
+                    "Working Set - Private",
+                    "Private Bytes",
+                    "Virtual Bytes"
+                };
+                List<long> results = new List<long>();
+/*
                 pc.CategoryName = "Process";
                 pc.CounterName = "Working Set - Private";
                 pc.InstanceName = currentProcess.ProcessName;
@@ -23,9 +30,21 @@ namespace Models_Resources
 
                 pc.Close();
                 pc.Dispose();
+*/
+                foreach(string counter in counters)
+                {
+                    results.Add(GetLongValuedPerformanceCounterForProcess(
+                                     counter, 
+                                     currentProcess));
+                }
 
                 processStats.ProcessName = currentProcess.ProcessName;
-                processStats.Memsize = memsize;
+                processStats.WorkingSet = results[0];
+                processStats.WorkingSetPrivate = results[1];
+                processStats.PrivateBytes = results[2];
+                processStats.VirtualBytes = results[3];
+
+                return processStats;
             }
             catch (Exception exception)
             {
@@ -35,8 +54,23 @@ namespace Models_Resources
 
                 throw new ResourceFindException(message);
             }
+        }
 
-            return processStats;
+        private long GetLongValuedPerformanceCounterForProcess(string name, Process currentProcess)
+        {
+            long result = 0;
+            PerformanceCounter pc = new PerformanceCounter();
+
+            pc.CategoryName = "Process";
+            pc.CounterName = name;
+            pc.InstanceName = currentProcess.ProcessName;
+
+            result = Convert.ToInt64(pc.NextValue()) / (long)1024;
+
+            pc.Close();
+            pc.Dispose();
+
+            return result;
         }
     }
 }
